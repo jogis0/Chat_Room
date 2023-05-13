076c9890c1df95a18b0c423d7b143cc6004cae9d
Jokūbas Viliušis <viliusisj@gmail.com>
2023-05-13 20:26:30 +0300

    Initial project with loaded javafx (correctly)

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.chat_room {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.example.chat_room to javafx.fxml;
-    exports com.example.chat_room;
-}
